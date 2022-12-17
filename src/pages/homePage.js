187d0be1df817e79a97097169c203a7ca0ff3920
mtabhay2271 <75633872+mtabhay2271@users.noreply.github.com>
2022-12-17 17:32:11 +0530

    feat: finally completed the basic setup

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-// import CollapsibleExample from '../components/layout/Navbar';
+import CollapsibleExample from '../components/layout/Navbar';
 // import Carousel from '../components/layout/Carousel';
 // import Cards from '../components/layout/Cards';
 
 const Home = () => {
   return (
     <>
+      <CollapsibleExample />
       <div className="outer">
         <h1>Home Page</h1>
       </div>
--- src/pages/new.js
+++ src/pages/new.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
